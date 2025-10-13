9371368bb28aa5ca5293f060767c9484a2c9c2bc
low <lowwenjuin27@gmail.com>
2025-10-14 00:35:03 +0800

    feat: practise bsq and gol

--- test/bsq/bsq.c
+++ test/bsq/bsq.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef struct s_bsq
 {
-	int line;
-	int width;
+	int line, width;
 	char empty, obst, full;
 	char **map;
-} t_bsq;
+}	t_bsq;
 
 int min3(int a, int b, int c)
 {
@@ -17,68 +17,53 @@ int min3(int a, int b, int c)
 int ft_strlen(char *s)
 {
 	int i = 0;
-	while (s[i])
+	while(s[i])
 		i++;
 	return i;
 }
 
 t_bsq *read_map(char *filename)
 {
-	//open file
 	FILE *file = filename ? fopen(filename, "r") : stdin;
 	if (!file) return NULL;
 
-	//malloc struct
 	t_bsq *map = malloc(sizeof(t_bsq));
-
-	//scan and assign first line
 	if (fscanf(file, "%d %c %c %c\n", &map->line, &map->empty, &map->obst, &map->full) != 4)
 	{
 		free(map);
-		fprintf(stderr, "Error: no line, emp, obst, full");
-		if(filename) fclose(file);
+		if(filename)
+			fclose(file);
 		return NULL;
 	}
 
-	//malloc map and init width
-	map->map = malloc(map->line * sizeof(char *));
+	map->map = malloc(sizeof(char *) * map->line);
 	map->width = 0;
-
-	//read file
 	for (int i = 0; i < map->line; i++)
 	{
-		//read each line
 		char *line = NULL;
 		size_t len = 0;
-		if(getline(&line, &len,file) == -1)
+		if (getline(&line, &len, file) == -1)
 		{
 			free(map);
-			fprintf(stderr, "Error: no read line");
-			if(filename) fclose(file);
+			if(filename)
+				fclose(file);
 			return NULL;
 		}
-
-		//convert new line to end line
 		int line_len = ft_strlen(line);
-		if(line[line_len - 1] == '\n')
+		if (line[line_len - 1] == '\n')
 			line[line_len - 1] = '\0';
-
-		//check all line equal
 		if (i == 0)
 			map->width = ft_strlen(line);
 		else if ((int)ft_strlen(line) != map->width)
 		{
 			free(map);
-			fprintf(stderr, "Error: not same size");
-			if(filename) fclose(file);
+			if(filename)
+				fclose(file);
 			return NULL;
 		}
-
-		// assign to map
 		map->map[i] = line;
 	}
 
-	//close file after read
 	if (filename)
 		fclose(file);
 
@@ -87,13 +72,10 @@ t_bsq *read_map(char *filename)
 
 int validate_map(t_bsq *map)
 {
-	//check struct, line, width
-	if(!map || map->line <= 0 || map->width <= 0)
+	if (!map || map->line <= 0 || map->width <= 0)
 		return 0;
-	//check obs, empty, full
 	if (map->empty == map->obst || map->empty == map->full || map->obst == map->full)
 		return 0;
-	//check map is obs and empty
 	for (int y = 0; y < map->line; y++)
 		for (int x = 0; x < map->width; x++)
 		{
@@ -106,78 +88,72 @@ int validate_map(t_bsq *map)
 
 void solve_bsq(t_bsq *map)
 {
-	//assign the map to all 0
 	int **dp = malloc(sizeof(int *) * map->line);
 	for (int i = 0; i < map->line; i++)
 		dp[i] = calloc(map->width, sizeof(int));
 
-	int max_size = 0, best_y = 0, best_x = 0;
+	int max_size = 0;
+	int best_y = 0;
+	int best_x = 0;
 
-	// loop the map and find the biggest square
 	for (int y = 0; y < map->line; y++)
 	{
 		for (int x = 0; x < map->width; x++)
 		{
-			//check place able
-			if (map->map[y][x] == map->obst) //if hit obst mean the position had block
+			if (map->map[y][x] == map->obst)
 				dp[y][x] = 0;
-			else if (y == 0 || x == 0) //if is first position then place 1
+			else if (y == 0 | x == 0)
 				dp[y][x] = 1;
-			else //the fill end here
+			else
 				dp[y][x] = min3(dp[y-1][x], dp[y][x-1], dp[y-1][x-1]) + 1;
 
-			//update biggest square found
 			if (dp[y][x] > max_size)
 			{
-				max_size = dp[y][x]; //get the biggest size
-				best_y = y; //
+				max_size = dp[y][x];
+				best_y = y;
 				best_x = x;
 			}
 		}
 	}
 
-	// fill the biggest squre to the possition
 	int start_y = best_y - max_size + 1;
 	int start_x = best_x - max_size + 1;
 	for (int i = start_y; i < start_y + max_size; i++)
 		for (int j = start_x; j < start_x + max_size; j++)
 			map->map[i][j] = map->full;
 
-	for(int i = 0; i < map->line; i++)
+	for (int i = 0; i < map->line; i++)
 		free(dp[i]);
 	free(dp);
-}
-
-void print_map(t_bsq *map)
-{
-	for (int i = 0; i < map->line; i++)
-		fprintf(stdout, "%s\n", map->map[i]);
 }
 
 void free_map(t_bsq *map)
 {
 	if (!map) return;
-	//free map
 	for (int i = 0; i < map->line; i++)
 		free(map->map[i]);
 	free(map->map);
-	//free struct
 	free(map);
+}
+
+void print_map(t_bsq *map)
+{
+	if (!map) return;
+	for (int i = 0; i < map->line; i++)
+		fprintf(stdout, "%s\n", map->map[i]);
 }
 
 void process_file(char *filename)
 {
-	t_bsq *map = read_map(filename); // read map
-	if(!map || !validate_map(map)) //check valid map
+	t_bsq *map = read_map(filename);
+	if (!map || !validate_map(map))
 	{
-		fprintf(stderr,"map error\n");
-		if (map)
-			free_map(map);
+		free_map(map);
 		return;
 	}
 	solve_bsq(map);
 	print_map(map);
-	free_map(map); //free map and struct
+	free_map(map);
 }
 
 int main(int ac, char **av)
@@ -191,3 +167,4 @@ int main(int ac, char **av)
 	}
 	return 0;
 }
+
--- test/game_of_life/game_of_lif.c
+++ test/game_of_life/game_of_lif.c
@@ -1,6 +1,6 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int ac, char **av)
 {
@@ -17,15 +17,15 @@ int main(int ac, char **av)
 	int y = 1;
 	int x = 1;
 	int pen = 0;
-	int grid[2][height + 2][width + 2];
+	int grid[2][height+2][width+2];
 	char c;
 
-	for (int d = 0; d < 2; d++)
-		for(int y = 0; y < height + 2; y++)
+	for(int d = 0; d < 2; d++)
+		for (int y = 0; y < height + 2; y++)
 			for (int x = 0; x < width + 2; x++)
 				grid[d][y][x] = 0;
 
-	while(read(0, &c, 1) > 0)
+	while (read(0, &c, 1) > 0)
 	{
 		if (c == 'w' && y > 1)
 			y--;
@@ -46,21 +46,24 @@ int main(int ac, char **av)
 		int cur = it % 2;
 		int next = (it + 1) % 2;
 
-		for (int y = 0; y < height; y++)
-			for (int x = 0; x < width; x++)
+		for (int y = 0; y < height + 2; y++)
+			for (int x = 0; x < width + 2; x++)
 				grid[next][y][x] = 0;
 
-		for (int y = 1; y <= height; y++) {
+		for (int y = 1; y <= height; y++)
+		{
 			for (int x = 1; x <= width; x++)
 			{
 				int n = 0;
 				for (int dy = -1; dy <= 1; dy++)
-					for(int dx = -1; dx <= 1; dx++)
+				{
+					for (int dx = -1; dx <= 1; dx++)
 					{
-						if (dx != 0 || dy != 0)
-							n += grid[cur][y + dy][x + dx];
+						if (dy != 0 || dx != 0)
+							n += grid[cur][dy+y][dx+x];
 					}
-				if (grid[cur][y][x] == 1)
+				}
+				if (grid[cur][y][x])
 					grid[next][y][x] = (n == 2 || n == 3);
 				else
 					grid[next][y][x] = (n == 3);
@@ -69,10 +72,11 @@ int main(int ac, char **av)
 	}
 
 	int final = iterate % 2;
-	for (int y = 1; y <= height; y++) {
-		for (int x = 1; x <= width; x++)
+	for (int y = 0; y < height + 2 ; y++)
+	{
+		for (int x = 0; x < width + 2; x++)
 		{
-			if (grid[final][y][x])
+			if (grid[final][y][x] == 1)
 				putchar('0');
 			else
 				putchar(' ');
